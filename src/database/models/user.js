6f22c3d167587ea753b43437c5e46dd9a116b8b3
gracio <gustavogracio@hotmail.com>
2022-07-13 16:38:35 -0300

    Requisito 2 completo

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  displayName: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+};
+
+module.exports = (sequelize) => {
+  const users = sequelize.define('User', attributes, { tableName: 'users' });
+  return users;
+};
